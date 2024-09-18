40d5b5a2f38109a6ab871ce69a0d36478d88dd5f
ff137 <ff137@proton.me>
2024-09-18 16:39:15 +0300

    :rocket: take on NATS plugin
    Copied from redis_events plugin, and replaced redis with nats-py (and json with orjson)
    
    Signed-off-by: ff137 <ff137@proton.me>

---
+++ nats_events/README.md
@@ -0,0 +1,22 @@
+# NATS Events Plugin
+
+## Overview
+
+Welcome to the NATS Events Plugin! This plugin is designed to facilitate event handling using NATS as the messaging system. It is currently under active development, and we are working hard to implement all the necessary features and ensure robust functionality.
+
+## Status
+
+**Under Construction** 🚧
+
+Please note that this plugin is still in the early stages of development. The following components are yet to be implemented:
+
+- **Inbound Transporters**: Mechanisms to receive and process incoming messages.
+- **Outbound Transporters**: Mechanisms to send messages to external systems.
+- **Documentation**: Comprehensive documentation to guide users on setup, configuration, and usage.
+- **Testing**: Thorough testing to ensure reliability and performance.
+
+## Features
+
+- **Event Handling**: Push events to NATS topics.
+- **Dynamic Topic Mapping**: Map event patterns to NATS topics using templates.
+- **Metadata Enrichment**: Add metadata to events before publishing.
---
+++ nats_events/nats_events/v1_0/nats_queue/events/__init__.py
@@ -0,0 +1,180 @@
+"""ACA-Py Events to NATS."""
+
+import base64
+import logging
+import re
+import time
+from string import Template
+from typing import Any, Optional, cast
+
+import orjson
+from aries_cloudagent.config.injection_context import InjectionContext
+from aries_cloudagent.core.event_bus import Event, EventBus, EventWithMetadata
+from aries_cloudagent.core.profile import Profile
+from aries_cloudagent.core.util import SHUTDOWN_EVENT_PATTERN, STARTUP_EVENT_PATTERN
+from aries_cloudagent.transport.error import TransportError
+from nats.aio.client import Client as NATS
+from nats.aio.errors import ErrConnectionClosed, ErrNoServers, ErrTimeout
+
+from ..config import EventConfig, OutboundConfig, get_config
+
+LOGGER = logging.getLogger(__name__)
+
+
+async def setup(context: InjectionContext):
+    """Setup the plugin."""
+    LOGGER.info("> Setting up NATS plugin")
+    config = get_config(context.settings).event or EventConfig.default()
+
+    bus = context.inject(EventBus)
+    if not bus:
+        raise ValueError("EventBus missing in context")
+
+    for event in config.event_topic_maps.keys():
+        LOGGER.info(f"subscribing to event: {event}")
+        bus.subscribe(re.compile(event), handle_event)
+
+    bus.subscribe(STARTUP_EVENT_PATTERN, on_startup)
+    bus.subscribe(SHUTDOWN_EVENT_PATTERN, on_shutdown)
+    LOGGER.info("< Successfully set up NATS plugin.")
+
+
+RECORD_RE = re.compile(r"acapy::record::([^:]*)(?:::(.*))?")
+WEBHOOK_RE = re.compile(r"acapy::webhook::{.*}")
+
+
+async def nats_setup(profile: Profile, event: Event) -> NATS:
+    """Connect, setup and return the NATS instance."""
+    connection_url = (get_config(profile.settings).connection).connection_url
+    LOGGER.info(f"Connecting to NATS url: {connection_url}")
+    nats = NATS()
+    try:
+        await nats.connect(servers=[connection_url])
+        LOGGER.info("NATS connection established.")
+        profile.context.injector.bind_instance(NATS, nats)
+    except (ErrConnectionClosed, ErrTimeout, ErrNoServers) as err:
+        LOGGER.error(f"Caught error in NATS setup: {err}")
+        raise TransportError(f"No NATS instance setup, {err}")
+    return nats
+
+
+async def on_startup(profile: Profile, event: Event):
+    """Setup NATS on startup."""
+    LOGGER.info("Setup NATS on startup")
+    await nats_setup(profile, event)
+    LOGGER.info("Successfully setup NATS.")
+
+
+async def on_shutdown(profile: Profile, event: Event):
+    """Called on shutdown."""
+    LOGGER.info("NATS plugin shutdown hook called.")
+    nats = profile.inject_or(NATS)
+    if nats:
+        await nats.close()
+        LOGGER.info("NATS connection closed.")
+
+
+def _derive_category(topic: str):
+    match = RECORD_RE.match(topic)
+    if match:
+        return match.group(1)
+    if WEBHOOK_RE.match(topic):
+        return "webhook"
+
+
+def process_event_payload(event_payload: Any):
+    """Process event payload."""
+    processed_event_payload = None
+    if isinstance(event_payload, dict):
+        processed_event_payload = event_payload
+    else:
+        processed_event_payload = orjson.loads(event_payload)
+    return processed_event_payload
+
+
+async def handle_event(profile: Profile, event: EventWithMetadata):
+    """Push events from aca-py events."""
+    config_events = get_config(profile.settings).event or EventConfig.default()
+    pattern = event.metadata.pattern.pattern
+    template = config_events.event_topic_maps.get(pattern)
+
+    if not template:
+        LOGGER.warning(f"Could not infer template from pattern: {pattern}")
+        return
+
+    if "-with-state" not in template:
+        # We are only interested in state change webhooks. This avoids duplicate events
+        return
+
+    nats = profile.inject_or(NATS)
+    if not nats:
+        nats = await nats_setup(profile, event)
+
+    LOGGER.debug("Handling event: %s", event)
+    wallet_id = cast(Optional[str], profile.settings.get("wallet.id"))
+    try:
+        event_payload = process_event_payload(event.payload)
+    except TypeError:
+        try:
+            event_payload = event.payload.serialize()
+        except AttributeError:
+            try:
+                event_payload = process_event_payload(event.payload.payload)
+            except TypeError:
+                event_payload = process_event_payload(event.payload.enc_payload)
+    payload = {
+        "wallet_id": wallet_id or "base",
+        "state": event_payload.get("state"),
+        "topic": event.topic,
+        "category": _derive_category(event.topic),
+        "payload": event_payload,
+    }
+    webhook_urls = profile.settings.get("admin.webhook_urls")
+    try:
+        nats_topic = Template(template).substitute(**payload)
+        LOGGER.debug(f"Sending message {payload} with topic {nats_topic}")
+
+        origin = profile.settings.get("default_label")
+        group_id = profile.settings.get("wallet.group_id")
+
+        metadata = {"time_ns": time.time_ns()}
+        metadata_wallet_id = {"x-wallet-id": wallet_id} if wallet_id else {}
+        metadata_group_id = {"group_id": group_id} if group_id else {}
+        metadata_origin = {"origin": origin} if origin else {}
+        metadata.update(metadata_wallet_id)
+        metadata.update(metadata_group_id)
+        metadata.update(metadata_origin)
+
+        outbound = orjson.dumps({"payload": payload, "metadata": metadata})
+        await nats.publish(nats_topic, outbound)
+
+        # Deliver/dispatch events to webhook_urls directly
+        if config_events.deliver_webhook and webhook_urls:
+            config_outbound = (
+                get_config(profile.settings).outbound or OutboundConfig.default()
+            )
+            for endpoint in webhook_urls:
+                api_key = None
+                if len(endpoint.split("#")) > 1:
+                    endpoint_hash_split = endpoint.split("#")
+                    endpoint = endpoint_hash_split[0]
+                    api_key = endpoint_hash_split[1]
+                webhook_topic = config_events.event_webhook_topic_maps.get(event.topic)
+                if endpoint and webhook_topic:
+                    endpoint = f"{endpoint}/topic/{webhook_topic}/"
+                    headers = {"x-wallet-id": wallet_id} if wallet_id else {}
+                    if api_key is not None:
+                        headers["x-api-key"] = api_key
+                    outbound_msg = {
+                        "service": {"url": endpoint},
+                        "payload": base64.urlsafe_b64encode(
+                            orjson.dumps(payload)
+                        ).decode(),
+                        "headers": headers,
+                    }
+                    await nats.publish(
+                        config_outbound.acapy_outbound_topic,
+                        orjson.dumps(outbound_msg),
+                    )
+    except (ErrConnectionClosed, ErrTimeout, ErrNoServers, ValueError) as err:
+        LOGGER.exception(f"Failed to process and send webhook, {err}")
