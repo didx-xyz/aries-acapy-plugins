88ce0348b85afd6c322f24a1f1bc3ae813936fae
ff137 <ff137@proton.me>
2024-10-31 21:25:01 +0300

    :art: reduce verbose debug logs
    Signed-off-by: ff137 <ff137@proton.me>

--- nats_events/nats_events/v1_0/nats_queue/config.py
+++ nats_events/nats_events/v1_0/nats_queue/config.py
@@ -158,7 +158,4 @@ def get_config(settings: Mapping[str, Any]) -> NATSConfig:
         config = NATSConfig.default()
 
     LOGGER.debug("Returning config: %s", config.model_dump_json(indent=2))
-    LOGGER.debug(
-        "Returning config(aliases): %s", config.model_dump_json(by_alias=True, indent=2)
-    )
     return config
