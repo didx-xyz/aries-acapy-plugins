5ad0c4063759ab4e3ae9b0fda93d86b0aaf3ce7d
cl0ete <cloete.dupreez@gmail.com>
2024-11-07 10:48:27 +0200

    update error logging

--- nats_events/nats_events/v1_0/nats_queue/events/__init__.py
+++ nats_events/nats_events/v1_0/nats_queue/events/__init__.py
@@ -84,7 +84,7 @@ async def nats_jetstream_setup(profile: Profile, event: Event) -> JetStreamConte
 
 async def error_callback(e):
     """Error callback for NATS."""
-    LOGGER.error("NATS error: {}", str(e))
+    LOGGER.error("NATS error: {}".format(e))
 
 
 async def disconnected_callback():
